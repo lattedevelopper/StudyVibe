91260f133b5249f6b5c7e47b504c3a4316c4c917
Laywood <pomilli911@gmail.com>
2025-09-04 13:04:26 +0700

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { CustomButton } from "@/components/ui/custom-button";
 
 const NotFound = () => {
   const location = useLocation();
